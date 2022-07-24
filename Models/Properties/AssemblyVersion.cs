e4811c805dda4f6ddfbcddc4aa9157e73cbb0fdd
Jason Brider <jason.brider@gmail.com>
2022-07-24 12:00:41 +1000

    update version number for manual docker build

--- Models/Properties/AssemblyVersion.cs
+++ Models/Properties/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("2022.07.7064.1")]
+[assembly: AssemblyFileVersion("2022.07.7064.1")]
 [assembly: AssemblyCopyright("Copyright © APSIM Initiative 2022")]
