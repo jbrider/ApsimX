a524c61a496adadd8c6aa085adc657193ba5d699
Jason Brider <jason.brider@gmail.com>
2022-08-30 20:30:53 +1000

    testing type naming

--- APSIM.Shared/Utilities/ReflectionUtilities.cs
+++ APSIM.Shared/Utilities/ReflectionUtilities.cs
@@ -359,7 +359,7 @@
                     {
                         ContractResolver = new DynamicContractResolver(includePrivates, includeChildren),
                         ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-                        
+                        TypeNameAssemblyFormatHandling = TypeNameAssemblyFormatHandling.Simple,
                         TypeNameHandling = TypeNameHandling.Objects,
                     });
         }
